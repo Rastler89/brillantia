576b91559b7f2bd6520fe3d027a63d264a29732e
Rastler <rastler89@gmail.com>
2025-04-29 17:05:05 +0200

    [fix] hash no encontrado

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -14,6 +14,7 @@ use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Facades\Hash;
 
 class UserResource extends Resource
 {
